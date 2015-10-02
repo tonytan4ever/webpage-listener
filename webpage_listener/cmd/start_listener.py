52018973cf4dc9c4b5f583a80ade394b9531ddcb
tonytan4ever <tonytan198211@gmail.com>
2015-10-02 17:18:56 -0400

    run command in daemon mode

--- webpage_listener/cmd/start_listener.py
+++ webpage_listener/cmd/start_listener.py
@@ -1,3 +1,5 @@
+import os
+
 from oslo_config import cfg
 
 from webpage_listener.common import cli
@@ -27,5 +29,29 @@ CONF(prog='web-pages-listener')
 
 @cli.runnable
 def run():
+    if CONF.daemon:
+        zerofd = os.open('/dev/zero', os.O_RDONLY)
+        nullfd = os.open('/dev/null', os.O_WRONLY)
+
+        # Close the stdthings and reassociate them with a non terminal
+        os.dup2(zerofd, 0)
+        os.dup2(nullfd, 1)
+        os.dup2(nullfd, 2)
+
+        # Detach process context, this requires 2 forks.
+        try:
+            pid = os.fork()
+            if pid > 0:
+                os._exit(0)
+        except OSError:
+            os._exit(1)
+
+        try:
+            pid = os.fork()
+            if pid > 0:
+                os._exit(0)
+        except OSError:
+            os._exit(2)
+
     listener.main(CONF.list_of_urls, CONF.css_selector_expression,
                   CONF.polling_period, CONF.hook_module, True)
